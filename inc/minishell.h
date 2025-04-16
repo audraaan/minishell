047b1fd1a4dea2747a3c4913bb5f53372d748c00
Audran Larroye <alarroye@z1r11p3.42lyon.fr>
2025-04-16 15:18:49 +0200

    parse d env en lst et unset

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: alarroye <alarroye@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 13:48:52 by alarroye          #+#    #+#             */
-/*   Updated: 2025/04/14 11:51:03 by alarroye         ###   ########lyon.fr   */
+/*   Updated: 2025/04/16 13:07:23 by alarroye         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,24 @@
 typedef struct s_data
 {
 	int len_data; // le nb de truc quya ds la ligne de commande
-	int type; // genre si c un file une cmd un < | > >> << t a capter k 
-	char **content;
-	struct s_data *next;
-}
+	int type;     // genre si c un file une cmd un < | > >> << t a capter k
+	char			**content;
+	struct s_data	*next;
+}					t_data;
 
-int		ft_free_dtab(char **tab);
-void	ft_error_msg(char *arg, char *msg);
-int		ft_error(char *msg, char **path, char **dtab, int status);
-char	**parse_env(char **env);
-char	*search_path(char *cmd, char **path, int *error);
-int		ft_is_access(char *path_cmd, int *error);
+typedef struct s_lst
+{
+	char			*content;
+	struct s_lst	*next;
+}					t_lst;
+
+t_lst				*new_node(char *str);
+t_lst				*parse_env(char **envp);
+int					ft_free_dtab(char **tab);
+void				ft_error_msg(char *arg, char *msg);
+int					ft_error(char *msg, char **path, char **dtab, int status);
+char				**parse_path(char **env);
+char				*search_path(char *cmd, char **path, int *error);
+int					ft_is_access(char *path_cmd, int *error);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: alarroye <alarroye@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 14:03:00 by alarroye          #+#    #+#             */
-/*   Updated: 2025/04/14 13:53:06 by alarroye         ###   ########lyon.fr   */
+/*   Updated: 2025/04/16 15:13:34 by alarroye         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ int	ft_is_exec(char *path_cmd, int *error)
 	return (0);
 }
 
-char	**parse_env(char **env)
+char	**parse_path(char **env)
 {
 	char	*tab;
 	char	**path;
@@ -93,28 +93,23 @@ char	*search_path(char *cmd, char **path, int *error)
 	return (NULL);
 }
 
-void	ft_error_msg(char *arg, char *msg)
-{
-	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(arg, 2);
-	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(msg, 2);
-	ft_putstr_fd("\n", 2);
-}
-
 int	redirect_input(char *file, char **path)
 {
 	int	infile;
 
 	if (access(file, F_OK) != 0)
+	{
 		ft_error("No such file or directory", path, &file, 127);
+	}
 	else if (access(file, R_OK) != 0)
 		ft_error("Permission denied", path, &file, 1);
 	else
 	{
 		infile = open(file, O_RDONLY);
 		if (infile == -1)
+		{
 			ft_error("error open", path, &file, errno);
+		}
 		if (dup2(infile, STDIN_FILENO) == -1)
 		{
 			close(infile);
@@ -125,12 +120,21 @@ int	redirect_input(char *file, char **path)
 	return (0);
 }
 
+void	ft_error_msg(char *arg, char *msg)
+{
+	ft_putstr_fd("minishell: ", 2);
+	if (arg)
+		ft_putstr_fd(arg, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(msg, 2);
+	ft_putstr_fd("\n", 2);
+}
 int	ft_error(char *msg, char **path, char **dtab, int status)
 {
 	if (dtab)
 		ft_error_msg(dtab[0], msg);
 	else
-		ft_error_msg(dtab[0], msg);
+		ft_error_msg(" ", msg);
 	ft_free_dtab(path);
 	ft_free_dtab(dtab);
 	if (status != -1)
@@ -138,7 +142,130 @@ int	ft_error(char *msg, char **path, char **dtab, int status)
 	return (1);
 }
 
-int	main(int ac, char **av, char **env)
+t_lst	*new_node(char *str)
+{
+	t_lst	*node;
+
+	node = malloc(sizeof(t_lst));
+	if (!node)
+		return (NULL);
+	node->content = ft_strdup(str);
+	node->next = NULL;
+	return (node);
+}
+
+t_lst	*parse_env(char **envp)
+{
+	t_lst	*env;
+	t_lst	*tmp;
+	int		i;
+
+	i = 0;
+	// if (!envp || !(*envp))
+	// its la merde
+	env = new_node(envp[i]);
+	// if (!env)
+	// jsp faut faire k
+	tmp = env;
+	while (envp[++i])
+	{
+		tmp->next = new_node(envp[i]);
+		tmp = tmp->next;
+		// if (!tmp)
+		// jsp faut faire k
+	}
+	return (env);
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	t_lst	*env;
+	t_lst	*tmp_env;
+
+	(void)av;
+	(void)ac;
+	env = parse_env(envp);
+	tmp_env = env;
+	while (tmp_env)
+	{
+		printf("%s\n", tmp_env->content);
+		tmp_env = tmp_env->next;
+	}
+	return (0);
+}
+
+int	ft_env(t_lst *env)
+{
+	t_lst	*tmp_env;
+
+	tmp_env = env;
+	while (tmp_env)
+	{
+		printf("%s\n", tmp_env->content);
+		tmp_env = tmp_env->next;
+	}
+	return (0);
+}
+void	ft_free_lst(t_lst *lst)
+{
+	t_lst	*tmp;
+
+	tmp = lst;
+	lst = lst->next;
+	if (tmp->content)
+		free(tmp->content);
+	if (tmp)
+		free(tmp);
+}
+
+int	check_params_unset(char **a, int *it)
+{
+	int	i;
+	int	j;
+
+	it = -1;
+	i = -1;
+	j = -1;
+	while (a[++i])
+	{
+		while (a[i][++j])
+			if (a[i][j] < 'A' || a[i][j] > 'Z')
+				return (1);
+		j = -1;
+	}
+	return (0);
+}
+
+void	ft_unset(t_lst **env, char **a)
+{
+	t_lst	*tmp_prev;
+	t_lst	*tmp_last;
+	int		i;
+
+	if (check_params_unset(a, &i))
+		return ;
+	while (a && a[++i])
+	{
+		tmp_prev = *env;
+		tmp_last = (*env)->next;
+		if (ft_strncmp(tmp_prev->content, a[i], ft_strlen(a[i])) == 0)
+			ft_free_lst(*env);
+		else
+			while (tmp_last)
+			{
+				if (ft_strncmp(tmp_last->content, a[i], ft_strlen(a[i])) == 0)
+				{
+					tmp_prev->next = tmp_last->next;
+					ft_free_lst(tmp_last);
+					break ;
+				}
+				tmp_prev = tmp_prev->next;
+				tmp_last = tmp_last->next;
+			}
+	}
+}
+
+int	main(int ac, char **av, char **envp)
 {
 	char	**cmd;
 	char	**path;
@@ -146,25 +273,37 @@ int	main(int ac, char **av, char **env)
 	char	*read;
 	int		status;
 	pid_t	pid;
-	//char	*file;
+	char	*file;
+	t_lst	*env;
 
+	// char	*tmp_cmd;
 	(void)ac;
 	(void)av;
+	env = parse_env(envp);
 	while (1)
 	{
-		path = parse_env(env);
+		file = NULL;
+		path = parse_path(env);
 		read = readline("minishell> ");
 		if (!read || (ft_strcmp(read, "exit") == 0))
+		{
 			break ;
+		}
 		if (!read[0])
 			continue ;
-		// printf("read=%s;\n",read);
 		if (ft_strchr(read, '<'))
-			read = &(ft_strchr(read, '<')[1]);
+			read = ft_strdup((ft_strchr(read, '<')));
 		cmd = ft_split(read, ' ');
+		printf("read=%s;\n", read);
 		free(read);
 		if (!cmd || !cmd[0])
 			return (ft_error("split error", path, cmd, -1));
+		if (ft_strchr(cmd[0], '<'))
+		{
+			file = cmd[1];
+			cmd += 2;
+		}
+		printf("cmd=%s;\n", cmd[0]);
 		path_cmd = search_path(cmd[0], path, &status);
 		if (!path_cmd)
 		{
@@ -178,7 +317,11 @@ int	main(int ac, char **av, char **env)
 			if (pid == -1)
 				return (ft_error("pid error", path, cmd, errno));
 			else if (pid == 0)
+			{
+				if (file)
+					redirect_input(cmd[1], path);
 				execve(path_cmd, cmd, env);
+			}
 			else
 				waitpid(pid, &status, 0);
 		}
